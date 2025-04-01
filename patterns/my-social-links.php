f28101f64a3df7fdf922ca59a60fd542ca657259
Janne Seppänen <j.v.seppanen@gmail.com>
2025-04-01 07:10:02 +0300

    Added My Social Links pattern

---
+++ patterns/my-social-links.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: My Social Links
+ * Slug: janneseppanen-theme-2025/my-social-links
+ * Categories: Buttons, Contact
+ */
+?>
+<!-- wp:social-links {"size":"has-normal-icon-size","className":"is-style-logos-only","style":{"spacing":{"blockGap":{"top":"12px","left":"12px"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<ul class="wp-block-social-links has-normal-icon-size is-style-logos-only"><!-- wp:social-link {"url":"https://github.com/jaseppan","service":"github"} /-->
+
+<!-- wp:social-link {"url":"https://www.linkedin.com/in/j-v-seppanen/","service":"linkedin"} /--></ul>
+<!-- /wp:social-links -->
